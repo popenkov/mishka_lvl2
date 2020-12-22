dbfd154a6b2609f1ae8edb523d6fe15acd2de7df
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-12-22 15:26:57 +0300

    js added

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,12 @@ const cleancss = require('gulp-clean-css');
 // Определяем логику работы Browsersync
 function browsersync() {
     browserSync.init({ // Инициализация Browsersync
-        server: { baseDir: 'source/' }, // Указываем папку сервера
+        /* server: { baseDir: 'source/' }, */  // Указываем папку сервера
         notify: false, // Отключаем уведомления
         online: true // Режим работы: true или false
     })
+
+    
 }
 
 // Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
--- source/js/modal-open.js
+++ source/js/modal-open.js
@@ -10,3 +10,40 @@ menuToggle.addEventListener('click', function () {
 
 console.log(menuToggle);
 console.log(modalMenuMobile);
+
+
+
+var orderButton = document.querySelector('.item-of-the-week__button');
+var modalOrderWindow = document.querySelector('.modal-window');
+var pageBodyContainer = document.querySelector('.body_container');
+var siteBody = document.querySelector('body');
+var ESCAPE_BUTTON = '27';
+
+var closePopUp = function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode == ESCAPE_BUTTON && modalOrderWindow.classList.contains('modal-window--show')) {
+        modalOrderWindow.classList.remove('modal-window--show');
+        pageBodyContainer.classList.remove('body_container--filter');
+    }
+
+    
+}
+/* var noScroll = function (evt) {
+window.addEventListener('scroll', (e) => {
+        window.scrollTo(0,0);
+      });
+} */
+
+
+var openPopUp = function (evt) {
+    evt.preventDefault();
+       
+    pageBodyContainer.classList.toggle('body_container--filter');
+    modalOrderWindow.classList.toggle('modal-window--show');
+    siteBody.classList.toggle('body__no-scroll');
+   
+}
+
+orderButton.addEventListener('click', openPopUp)
+document.addEventListener('keydown', closePopUp);
+
