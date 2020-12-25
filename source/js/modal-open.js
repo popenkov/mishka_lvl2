837dc8798931436c634c89df4a1c6d865b6d8922
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2020-12-25 11:18:28 +0300

    catalog started

--- source/js/modal-open.js
+++ source/js/modal-open.js
@@ -5,7 +5,7 @@ var modalClosure = document.querySelector('.main-nav__list__closure-button');
 
 menuToggle.addEventListener('click', function () {
     modalMenuMobile.classList.toggle('main-nav__wrapper--active');
- 
+
 })
 
 console.log(menuToggle);
@@ -17,33 +17,37 @@ var orderButton = document.querySelector('.item-of-the-week__button');
 var modalOrderWindow = document.querySelector('.modal-window');
 var pageBodyContainer = document.querySelector('.body_container');
 var siteBody = document.querySelector('body');
+var modalSendRequestButton = document.querySelector('.modal-window__button');
 var ESCAPE_BUTTON = '27';
 
 var closePopUp = function (evt) {
     evt.preventDefault();
+    modalOrderWindow.classList.remove('modal-window--show');
+    pageBodyContainer.classList.remove('body_container--filter');
+    siteBody.classList.remove('body__no-scroll');
+
+}
+
+var closePopUpOnEsc = function (evt) {
+    evt.preventDefault();
     if (evt.keyCode == ESCAPE_BUTTON && modalOrderWindow.classList.contains('modal-window--show')) {
         modalOrderWindow.classList.remove('modal-window--show');
         pageBodyContainer.classList.remove('body_container--filter');
+        siteBody.classList.remove('body__no-scroll');
     }
 
-    
 }
-/* var noScroll = function (evt) {
-window.addEventListener('scroll', (e) => {
-        window.scrollTo(0,0);
-      });
-} */
 
 
 var openPopUp = function (evt) {
     evt.preventDefault();
-       
+
     pageBodyContainer.classList.toggle('body_container--filter');
-    modalOrderWindow.classList.toggle('modal-window--show');
-    siteBody.classList.toggle('body__no-scroll');
-   
+    modalOrderWindow.classList.add('modal-window--show');
+    siteBody.classList.add('body__no-scroll');
+
 }
 
 orderButton.addEventListener('click', openPopUp)
-document.addEventListener('keydown', closePopUp);
-
+document.addEventListener('keydown', closePopUpOnEsc);
+modalSendRequestButton.addEventListener('click', closePopUp);
