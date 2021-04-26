70ddcb373ac0b4fc8d856e12f626ed2ad4263790
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-04-26 22:42:17 +0300

    form changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,36 @@
 const gulp = require("gulp");
-
 const plumber = require("gulp-plumber");
-
 const sourcemap = require("gulp-sourcemaps");
-
 const sass = require('gulp-sass');
-
 sass.compiler = require('node-sass');
-
 const postcss = require("gulp-postcss");
-
 const autoprefixer = require("autoprefixer");
-
 const csso = require("postcss-csso");
-
 const rename = require("gulp-rename");
-
 const htmlmin = require("gulp-htmlmin");
-
 const terser = require("gulp-terser");
-
 const imagemin = require("gulp-imagemin");
-
 const webp = require("gulp-webp");
-
 const svgstore = require("gulp-svgstore");
-
 const del = require("del");
-
 const sync = require("browser-sync").create();
-
 const ghPages = require('gulp-gh-pages');
 
 // Styles
 
 const styles = () => {
-
-return gulp.src("source/sass/style.scss")
-
-.pipe(plumber())
-
-.pipe(sourcemap.init())
-
-.pipe(sass().on('error', sass.logError))
-
-.pipe(postcss([
-
-autoprefixer(),
-
-csso()
-
-]))
-
-.pipe(rename("style.min.css"))
-
-.pipe(sourcemap.write("."))
-
-.pipe(gulp.dest("build/css"))
-
-.pipe(sync.stream());
-
+    return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+    autoprefixer(),
+    csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
 }
 
 exports.styles = styles;
@@ -67,51 +38,33 @@ exports.styles = styles;
 // HTML
 
 const html = () => {
-
-return gulp.src("source/*.html")
-
-.pipe(htmlmin({ collapseWhitespace: true }))
-
-.pipe(gulp.dest("build"));
-
-}
+    return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+    }
 
 // Scripts
 
 const scripts = () => {
-
-return gulp.src("source/js/bundle.js")
-
-.pipe(terser())
-
-.pipe(rename("script.min.js"))
-
-.pipe(gulp.dest("build/js"))
-
-.pipe(sync.stream());
-
-}
+    return gulp.src("source/js/bundle.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+    }
 
 exports.scripts = scripts;
 
 // Images
 
 const images = () => {
-
-return gulp.src("source/img/**/*.{png,jpg,svg}")
-
-.pipe(imagemin([
-
-imagemin.mozjpeg({progressive: true}),
-
-imagemin.optipng({optimizationLevel: 3}),
-
-imagemin.svgo()
-
+    return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
 ]))
-
-.pipe(gulp.dest("build/img"))
-
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.images = images;
@@ -119,13 +72,9 @@ exports.images = images;
 // WebP
 
 const createWebp = () => {
-
-return gulp.src("source/img/**/*.{jpg,png}")
-
-.pipe(webp({quality: 90}))
-
-.pipe(gulp.dest("build/img"))
-
+    return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.createWebp = createWebp;
@@ -133,15 +82,10 @@ exports.createWebp = createWebp;
 // Sprite
 
 const sprite = () => {
-
-return gulp.src("source/img/icons/*.svg")
-
-.pipe(svgstore())
-
-.pipe(rename("sprite.svg"))
-
-.pipe(gulp.dest("build/img"));
-
+    return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.sprite = sprite;
@@ -149,25 +93,15 @@ exports.sprite = sprite;
 // Copy
 
 const copy = (done) => {
-
-gulp.src([
-
-"source/fonts/*.{woff2,woff}",
-
-"source/*.ico",
-
-"source/img/**/*.{jpg,png,svg}",
-
-], {
-
-base: "source"
-
-})
-
-.pipe(gulp.dest("build"))
-
-done();
-
+    gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.{jpg,png,svg}",
+    ], {
+    base: "source"
+    })
+    .pipe(gulp.dest("build"))
+    done();
 }
 
 exports.copy = copy;
