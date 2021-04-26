10ec91882492baa4d0273136e2bda31adfeaf95d
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-04-26 21:56:12 +0300

    issue with modal window resolved

--- source/js/bundle.js
+++ source/js/bundle.js
@@ -1,121 +1,4 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./source/js/modules.js");
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ "./source/js/modules.js":
-/*!******************************!*\
-  !*** ./source/js/modules.js ***!
-  \******************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-window.addEventListener('DOMContentLoaded', function () {
-
-    "use strict";
-    let slider = __webpack_require__(/*! ./parts/slider.js */ "./source/js/parts/slider.js"),
-        popup = __webpack_require__(/*! ./parts/popup.js */ "./source/js/parts/popup.js");
-
-
-    popup();
-    slider();
-
-});
-
-
-/***/ }),
-
-/***/ "./source/js/parts/popup.js":
-/*!**********************************!*\
-  !*** ./source/js/parts/popup.js ***!
-  \**********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-function popup() {
+//popup
     var menuToggle = document.querySelector('.page-header__menu-toggle');
     var modalMenuMobile = document.querySelector('.main-nav__wrapper');
     var modalClosure = document.querySelector('.main-nav__list__closure-button');
@@ -124,7 +7,16 @@ function popup() {
     menuToggle.addEventListener('click', function () {
       modalMenuMobile.classList.toggle('main-nav__wrapper--active');
 
+      if (modalMenuMobile.classList.contains('main-nav__wrapper--active')) {
+        console.log('123')
+    window.addEventListener(`resize`, () => {
+        modalMenuMobile.classList.remove('main-nav__wrapper--active');
+      });
+    }
+
     })
+
+
 
 
     var orderButton = document.querySelector('.item-of-the-week__button');
@@ -166,21 +58,9 @@ function popup() {
     document.addEventListener('keydown', closePopUpOnEsc);
     modalSendRequestButton.addEventListener('click', closePopUp);
 
-}
-
-module.exports = popup;
 
 
-/***/ }),
-
-/***/ "./source/js/parts/slider.js":
-/*!***********************************!*\
-  !*** ./source/js/parts/slider.js ***!
-  \***********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-function slider() {
+//slider
     let slideIndex = 1;
     let slides = document.querySelectorAll('.reviews__item');
     let prev = document.querySelector('.slider__toggle--before');
@@ -218,12 +98,4 @@ function slider() {
       plusSlides(1);
     })
 
-}
 
-module.exports = slider;
-
-
-/***/ })
-
-/******/ });
-//# sourceMappingURL=bundle.js.map
